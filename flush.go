8926c70bc7a2a8dc202804d0c973b29539b0cdcd
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2021-03-24 11:56:00 +0000

    Use embeddable plugin API (#2)

--- flush.go
+++ flush.go
@@ -1,4 +1,4 @@
-package main
+package flush
 
 import (
 	"net/http"
@@ -8,20 +8,17 @@ import (
 	"github.com/riposo/riposo/pkg/plugin"
 )
 
-var _ plugin.Factory = Plugin
-
-// Plugin export definition.
-func Plugin(rts *api.Routes) (plugin.Plugin, error) {
-	rts.Method(http.MethodPost, "/__flush__", api.HandlerFunc(internal.Handler))
-
-	return plugin.New(
-		"flush_endpoint",
+func init() {
+	plugin.Register(plugin.New(
+		"flush",
 		map[string]interface{}{
 			"description": "The __flush__ endpoint can be used to remove all data from all backends.",
 			"url":         "https://github.com/riposo/flush",
 		},
+		func(rts *api.Routes) error {
+			rts.Method(http.MethodPost, "/__flush__", api.HandlerFunc(internal.Handler))
+			return nil
+		},
 		nil,
-	), nil
+	))
 }
-
-func main() {}
--- go.mod
+++ go.mod
@@ -1,11 +1,9 @@
 module github.com/riposo/flush
 
-go 1.14
-
-replace github.com/riposo/riposo => ../riposo
+go 1.15
 
 require (
 	github.com/onsi/ginkgo v1.14.2
 	github.com/onsi/gomega v1.10.4
-	github.com/riposo/riposo v0.0.0-20210226155134-b4e129732a1c
+	github.com/riposo/riposo v0.0.0-20210325154750-64868eb37850
 )
